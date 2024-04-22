10816bc1284a37cab4220f847513171abb110e87
satyammangroliya <mangroliyasatyam@gmail.com>
2024-04-22 23:27:30 +0200

    Update version

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@ require_once __DIR__ . "/vendor/autoload.php";
 $id = "xlto";
 $version = "0.1.3";
 $ilias_min_version = "5.4.0";
-$ilias_max_version = "6.999";
+$ilias_max_version = "7.999";
 $responsible = "Minervis GmbH";
 $learning_progress = 1;
 $responsible_mail = "jephte.abijuru@minervis.com";
